0f68c9116173adf7ac0f7822526610b87dca7779
Silvia-Wachira <slywachira96@gmail.com>
2023-08-01 05:05:57 +0300

    add a submit event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,5 +9,16 @@ let keyword = "";
 let page = 1;
 async function searchImages() {
      keyword = searchBox.value;
-     const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id={accessKey}';
+     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+     const response = await fetch(url);
+     const data = await response.json()
+
+     console.log(data)
 }
+
+searchForm.addEventListener('submit', (e) =>{
+     e.preventDefault();
+     page = 1;
+     searchImages();
+})
